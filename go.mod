601b9b98cf82c2494917e171ed6257f637869fa5
Artem Yurchenko <artem@studiovector.pl>
2020-07-04 16:06:24 +0200

    Initial commit

---
+++ go.mod
@@ -0,0 +1,5 @@
+module test-gaem
+
+go 1.14
+
+require github.com/gin-gonic/gin v1.6.3
---
+++ main.go
@@ -0,0 +1,21 @@
+package main
+
+import (
+	"github.com/gin-gonic/gin"
+	"log"
+	"net/http"
+)
+
+func main() {
+	//gin.SetMode(gin.ReleaseMode)
+
+	router := gin.Default()
+	router.Static("/StaticData", "./TgBuild/StaticData")
+	router.LoadHTMLGlob("./TgBuild/index.html")
+	router.GET("/", loadMainPage)
+	log.Panic(router.Run(":8080"))
+}
+
+func loadMainPage(context *gin.Context) {
+	context.HTML(http.StatusOK, "index.html", gin.H{})
+}
